b0ef5a74389db75ce0b20cf0317e99666b89d8af
Garrett Dodd <garrettdodd03@gmail.com>
2022-12-18 22:37:27 -0500

    port

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+app.set('port', process.env.PORT || 3000);
 app.get('/', (req, res) => 
     res.send('Hello World!')
 )
 
-app.listen(80);
+app.listen(port);
