066db3b2fff9ece053b8f8b117b7f2b4d278efb5
Garrett Dodd <garrettdodd03@gmail.com>
2022-12-29 01:12:35 -0500

    Merge branch 'main' of https://github.com/gdodd1/

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+        var dessert = 'hello';
+        console.log(__dirname + "index.ejs")
+        // res.render(__dirname + "index.ejs", {dessert:dessert});
+});
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
